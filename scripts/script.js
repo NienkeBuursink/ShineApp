e673a2004ffd795636405e0abdf2292ca0af3fa4
Nienke <152287217+NienkeBuursink@users.noreply.github.com>
2024-09-13 10:01:23 +0200

    voor de bespreking hihihi

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+const button = document.querySelector("button");
+const header = document.querySelector("header");
+const nav = document.querySelector("nav");
+
+button.onclick = toggleMenu;
+
+function toggleMenu (){
+  header.classList.toggle("toonMenu")
+}
