d35c8fa41c26fed39b2e718eb705f32635f9f8c0
Nienke <152287217+NienkeBuursink@users.noreply.github.com>
2024-09-20 21:15:19 +0200

    gekke review ding duurde te lang hahahahahahahaha

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,33 @@
 // JavaScript Document
-console.log("hi");
-
 const button = document.querySelector("button");
 const header = document.querySelector("header");
+const reviews = document.querySelectorAll(".reviews p");
 
 button.onclick = toggleMenu;
 
 function toggleMenu (){
   header.classList.toggle("toonMenu")
 }
+
+
+// tip gekregen van Tom en Bahaa 
+function reviewsLatenZien(currentIndex = 0) {
+  let alleReviews = reviews.length;
+
+  reviews.forEach((p) => {
+    p.classList.remove("latenZien");
+    p.classList.add("nietLatenZien");
+  });
+
+  reviews[currentIndex].classList.remove("nietLatenZien");
+  reviews[currentIndex].classList.add("latenZien");
+
+  let nextIndex = (currentIndex + 1) % alleReviews;
+
+  setTimeout(() => reviewsLatenZien(nextIndex), 7000);
+}
+
+
+window.onload = () => reviewsLatenZien();
+
+
